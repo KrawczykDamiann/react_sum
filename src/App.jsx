0da1d0b13d1cc45315412f61026b9a29bf019bc6
Damian Krawczyk <pirrat82@gmail.com>
2025-04-04 12:00:21 +0200

    task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './App.scss';
 import { Sum } from './components/Sum/Sum';
+import './App.scss';
 
 export const App = () => (
-  <>
+  <div className="app-container">
     <Sum a={2} b={3} />
     <Sum a={-5} b={5} />
     <Sum a={10} />
     <Sum b={5} />
     <Sum />
-  </>
+  </div>
 );
+
+export default App;
--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,4 +1,8 @@
-export const Sum = ({ a = 0, b = 0 }) => {
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+
+export const Sum = ({ a, b }) => {
   const sum = a + b;
 
   return (
@@ -6,4 +10,14 @@ export const Sum = ({ a = 0, b = 0 }) => {
       Sum of {a} and {b} is {sum}
     </p>
   );
+};
+
+Sum.propTypes = {
+  a: PropTypes.number,
+  b: PropTypes.number,
+};
+
+Sum.defaultProps = {
+  a: 0,
+  b: 0,
 };
